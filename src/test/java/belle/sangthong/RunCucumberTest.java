c05d7b46473d46c543817b339962cc43c0aff499
Belle Sangthong <belle.sangthong@iths.se>
2025-01-24 16:50:47 +0100

    Configure JUnit 5 to run Cucumber tests

--- src/test/java/belle/sangthong/RunCucumberTest.java
+++ src/test/java/belle/sangthong/RunCucumberTest.java
@@ -1,4 +1,15 @@
 package belle.sangthong;
 
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.SelectClasspathResource;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "belle.sangthong.stepdefinitions")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
 public class RunCucumberTest {
 }
