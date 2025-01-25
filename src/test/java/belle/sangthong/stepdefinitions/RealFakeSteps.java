8d6451bea6d1c6e010bef352b49fd435c242a967
Belle Sangthong <belle.sangthong@iths.se>
2025-01-25 10:06:23 +0100

    Instantiated a new QuizGame object inside the step definition

--- src/test/java/belle/sangthong/stepdefinitions/RealFakeSteps.java
+++ src/test/java/belle/sangthong/stepdefinitions/RealFakeSteps.java
@@ -1,4 +1,12 @@
 package belle.sangthong.stepdefinitions;
 
+import io.cucumber.java.en.Given;
+
 public class RealFakeSteps {
+
+    @Given("the user is playing the quiz game")
+    public void theUserIsPlayingTheQuizGame() {
+        QuizGame quizGame = new QuizGame();
+
+    }
 }
