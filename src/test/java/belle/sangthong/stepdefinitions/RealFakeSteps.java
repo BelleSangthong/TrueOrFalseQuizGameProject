dad578f4ce3f62e8ed29e7a3623c26f9eb5afc43
Belle Sangthong <belle.sangthong@iths.se>
2025-01-27 11:52:30 +0100

    Finished with given, when and then

--- src/test/java/belle/sangthong/stepdefinitions/RealFakeSteps.java
+++ src/test/java/belle/sangthong/stepdefinitions/RealFakeSteps.java
@@ -9,16 +9,16 @@ import io.cucumber.java.en.When;
 
 public class RealFakeSteps {
 
+    private QuizGame quizGame = new QuizGame();
+
     @Given("the user is playing the quiz game")
     public void theUserIsPlayingTheQuizGame() {
-        QuizGame quizGame = new QuizGame();
         quizGame.resetScore();
         quizGame.loadQuestions();
     }
 
     @When("the user answers the question correctly")
     public void theUserAnswersTheQuestionCorrectly() {
-        QuizGame quizGame = new QuizGame();
         Question question = quizGame.getCurrentQuestion();
         boolean correctAnswer = question.getCorrectAnswer();
         quizGame.answerQuestion(correctAnswer);
@@ -26,8 +26,28 @@ public class RealFakeSteps {
 
     @Then("the user should receive 1 point")
     public void theUserShouldReceive1Point() {
-        QuizGame quizGame = new QuizGame();
         int score = quizGame.getScore();
         assert score == 1;
     }
+
+    @Given("the user is playing the quiz game")
+    public void theUserIsPlayingTheQuizGameWithIncorrectAnswer() {
+        quizGame.resetScore();
+        quizGame.loadQuestions();
+    }
+
+    @When("the user answers the question incorrectly")
+    public void theUserAnswersTheQuestionIncorrectly() {
+        Question question = quizGame.getCurrentQuestion();
+        boolean incorrectAnswer = !question.getCorrectAnswer();
+        quizGame.answerQuestion(incorrectAnswer);
+    }
+
+    @Then("the user should receive 0 points")
+    public void theUserShouldReceive0Points() {
+        int score = quizGame.getScore();
+        assert score == 0;
+    }
+
+
 }
