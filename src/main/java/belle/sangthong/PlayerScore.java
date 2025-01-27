7acfbdc65f779ca5efc03ce6bcf65a14a69cacd5
Belle Sangthong <belle.sangthong@iths.se>
2025-01-27 18:37:06 +0100

    Implemented the code that handle user/player's score

--- src/main/java/belle/sangthong/PlayerScore.java
+++ src/main/java/belle/sangthong/PlayerScore.java
@@ -1,4 +1,17 @@
 package belle.sangthong;
 
 public class PlayerScore {
+    private int score;
+
+    public PlayerScore() {
+        this.score = 0;
+    }
+
+    public void addPoint() {
+        score++;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
